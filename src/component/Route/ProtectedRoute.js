4b56dfadc5cf93ee87a9993b471b2e31502a083c
Mukul <mukulaggarwal606@gmail.com>
2022-12-19 16:15:26 +0530

    frontend added

---
+++ src/component/Route/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom';
+import {useSelector} from "react-redux";
+
+const ProtectedRoute = ({ isAdmin, component:Component, ...rest}) => {
+  
+    const {loading, isAuthenticated, user} = useSelector(state=> state.user);
+  
+    return (
+    <>
+        {loading === false && (
+            <Route
+            {...rest}
+            render={(props)=>{
+                if(isAuthenticated === false){
+                    return <Redirect to ="/login" />;
+                }
+                if(isAdmin === true && user.role!== "admin"){
+                    return <Redirect to ="/login" />;
+                }
+                return <Component {...props} />;
+            }}
+            />
+        )}
+    </>
+  )
+}
+
+export default ProtectedRoute
